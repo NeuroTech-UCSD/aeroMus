c0a65a3ad4fb4dad916c8ff4cee20380c43fc8be
SM-Figueroa <s2figuer@ucsd.edu>
2023-05-23 14:26:44 -0700

    task: update to plot multiple signals with legend (still slow) needs fixing

--- utils/signal_processing.py
+++ utils/signal_processing.py
@@ -1,39 +1,9 @@
 import numpy as np
 
+def plot_realtime_emg(lsl_stream, num_streams=1, width=50, laplacian_transform=False):
 
-def laplacian_transform(emg_sig):
-    """
-    Calculates laplacian transform of emg_signal.
-
-    Parameters
-    ----------
-    emg_sig: 2D array (n_electrodes, n_times)
-
-    Returns
-    -------
-    laplacian_signal: 1D laplacian signal
-    """
-
-    electrode_measurements = np.abs(emg_sig) # "rectify signal"
-
-    laplacian_signal = []
-    for i in range(1, len(electrode_measurements) - 1):
-        for j in range(1, len(electrode_measurements[i]) - 1):
-
-            # calculate lapacian weighting central electrode as 4 and others as 1
-            laplacian_value = electrode_measurements[i-1][j] + \
-            electrode_measurements[i+1][j] + electrode_measurements[i][j-1] + \
-            electrode_measurements[i][j+1] - 4 * electrode_measurements[i][j]
-
-            laplacian_signal.append(laplacian_value)
-
-    return laplacian_signal
-
-
-
-import matplotlib.pyplot as plt
-
-def plot_realtime_emg(lsl_stream, laplacian_transform=False):
+    import matplotlib.pyplot as plt
+    from collections import deque
     """
     Plots emg signal in real time.
 
@@ -48,43 +18,59 @@ def plot_realtime_emg(lsl_stream, laplacian_transform=False):
     
     # Set up the figure and axis
     fig, ax = plt.subplots()
-    line, = ax.plot([], [])  # Create an empty line object for updating
 
     # Set the plot properties
-    ax.set_xlim(0, 1000)  # Adjust the x-axis limits as needed
-    ax.set_ylim(-300, 300)   # Adjust the y-axis limits as needed
+    ax.set_xlim(0, width)  # Adjust the x-axis limits as needed
+    ax.set_ylim(-100, 100)   # Adjust the y-axis limits as needed
     ax.set_xlabel('Time')
     ax.set_ylabel('EMG Signal')
     ax.set_title('Real-time EMG Signal Plot')
+    legend_labels = [f'Stream {i+1}' for i in range(num_streams)]
 
     # Initialize the data
-    x_data = []
-    y_data = []
+    x_data = [] # deque(width*[0], width)
+    y_data = [[] for _ in range(num_streams)] # [deque(width*[0], width) for _ in range(num_streams)] #deque(maxlen=width)
+
+    lines = []
+
+
+    # Create the line objects for each stream
+    for _ in range(num_streams):
+        line, = ax.plot([], [])  # Create an empty line object for each stream
+        lines.append(line)
 
     # Function to update the plot with new data
-    def update_plot(new_x, new_y):
+    def update_plot(new_x, new_ys, width):
         x_data.append(new_x)
-        y_data.append(new_y)
-        line.set_data(x_data, y_data)
+
+        for i, new_y in enumerate(new_ys):
+            y_data[i].append(new_y)
+            lines[i].set_data(x_data, y_data[i])
+        ax.set_xlim(max(0,len(x_data)-width), len(x_data))
         ax.figure.canvas.draw()
+
 
     # Start the real-time plotting
     plt.ion()  # Turn on interactive mode for continuous updating
 
     # Loop for real-time data acquisition and plotting
+    counter = 0
     while True:
+        ax.legend(lines, legend_labels)
+
         # Get the latest sample from the LSL stream
         sample, timestamp = lsl_stream.pull_sample(timeout=0.0)
         if sample is not None:
             # choose 2 random samples to perform Laplacian
             if laplacian_transform:
-                output_signal = bipolar_laplacian(sample[0],sample[1])
-            else:
-                output_signal = sample[5]  # Modify this based on the structure of LSL stream data
-            update_plot(len(x_data), output_signal)
+                laplacian_signal = bipolar_laplacian(sample[0],sample[1])
+
+            update_plot(counter, sample[:num_streams], width)
+
+        counter += 1
 
         # Pause for a small interval to allow the plot to update
-        plt.pause(0.001)
+        plt.pause(0.0001)
 
     # Stop the real-time plotting
     plt.ioff()  # Turn off interactive mode
