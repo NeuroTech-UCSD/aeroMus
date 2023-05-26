37730904cf302daec9ed8d24a26721745ded9b6b
SM-Figueroa <s2figuer@ucsd.edu>
2023-05-26 15:01:54 -0700

    feat: lsl plotting a bit faster

--- utils/signal_processing.py
+++ utils/signal_processing.py
@@ -1,38 +1,29 @@
 import numpy as np
 
-def plot_realtime_emg(lsl_stream, num_streams=1, width=50, laplacian_transform=False):
+
+x_min, x_max = 0, 300
+
+def plot_realtime_data(inlet, num_streams, frames=10000, interval=10):
 
     import matplotlib.pyplot as plt
-    from collections import deque
-    """
-    Plots emg signal in real time.
+    from matplotlib.animation import FuncAnimation
+    import numpy as np
+    import time
 
-    Parameters
-    ----------
-    lsl_stream: lsl stream object
-
-    Returns
-    -------
-    None
-    """
-    
     # Set up the figure and axis
     fig, ax = plt.subplots()
+    lines = []  # Create empty line objects for each stream
 
     # Set the plot properties
-    ax.set_xlim(0, width)  # Adjust the x-axis limits as needed
-    ax.set_ylim(-100, 100)   # Adjust the y-axis limits as needed
+    ax.set_xlim(x_min, x_max)  # Adjust the x-axis limits as needed
+    ax.set_ylim(-200, 200)   # Adjust the y-axis limits as needed
     ax.set_xlabel('Time')
-    ax.set_ylabel('EMG Signal')
-    ax.set_title('Real-time EMG Signal Plot')
-    legend_labels = [f'Stream {i+1}' for i in range(num_streams)]
+    ax.set_ylabel('Signal')
+    ax.set_title('Real-time Data Plot')
 
     # Initialize the data
-    x_data = [] # deque(width*[0], width)
-    y_data = [[] for _ in range(num_streams)] # [deque(width*[0], width) for _ in range(num_streams)] #deque(maxlen=width)
-
-    lines = []
-
+    x_data = [[] for _ in range(num_streams)]
+    y_data = [[] for _ in range(num_streams)]  # Create empty lists for each stream
 
     # Create the line objects for each stream
     for _ in range(num_streams):
@@ -40,43 +31,31 @@ def plot_realtime_emg(lsl_stream, num_streams=1, width=50, laplacian_transform=F
         lines.append(line)
 
     # Function to update the plot with new data
-    def update_plot(new_x, new_ys, width):
-        x_data.append(new_x)
+    def update_plot(frame):
+        global x_min, x_max
+        sample, _ = inlet.pull_sample()
+        for i in range(num_streams):
+            # Get the latest sample from each LSL stream
+            signal = sample[i]  # Modify this based on the structure of your LSL stream data
+            x_data[i].append(frame)
+            y_data[i].append(signal)
+            lines[i].set_data(x_data[i], y_data[i])
+        x_max += 1
+        x_min = max(0, x_max-400)
+        ax.set_xlim(x_min, x_max)
 
-        for i, new_y in enumerate(new_ys):
-            y_data[i].append(new_y)
-            lines[i].set_data(x_data, y_data[i])
-        ax.set_xlim(max(0,len(x_data)-width), len(x_data))
-        ax.figure.canvas.draw()
-
+        return lines
 
     # Start the real-time plotting
-    plt.ion()  # Turn on interactive mode for continuous updating
+    anim = FuncAnimation(fig, update_plot, frames=frames, interval=interval, blit=True)
 
-    # Loop for real-time data acquisition and plotting
-    counter = 0
-    while True:
-        ax.legend(lines, legend_labels)
+    # Update the legend with the stream labels
+    legend_labels = [f'Stream {i+1}' for i in range(num_streams)]
+    ax.legend(lines, legend_labels)
 
-        # Get the latest sample from the LSL stream
-        sample, timestamp = lsl_stream.pull_sample(timeout=0.0)
-        if sample is not None:
-            # choose 2 random samples to perform Laplacian
-            if laplacian_transform:
-                laplacian_signal = bipolar_laplacian(sample[0],sample[1])
-
-            update_plot(counter, sample[:num_streams], width)
-
-        counter += 1
-
-        # Pause for a small interval to allow the plot to update
-        plt.pause(0.0001)
-
-    # Stop the real-time plotting
-    plt.ioff()  # Turn off interactive mode
-
-    # Show the plot (optional)
+    # Show the plot
     plt.show()
+
 
 
 def bipolar_laplacian(V_d, V_o, r=3.0):
