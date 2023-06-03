ad390572837a8d1d9a31ced7510490c7fc8d7579
SM-Figueroa <s2figuer@ucsd.edu>
2023-06-03 14:46:22 -0700

    chore: create some feature functions for ML use

---
+++ utils/features.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+
+### Time-Domain Features
+
+def mean_absolute_value(data):
+    return np.mean(np.abs(data))
+
+def slope_sign_changes(data):
+    return np.diff((np.diff(data) < 0)).sum() # astypefloatnecessary?
+
+def waveform_length(data):
+    return np.sum(np.abs(np.diff(data)))
+
+def zero_crossings(data):
+    return np.sum(np.diff(data > 0).astype(float)) # astypefloatnecessary?
+
+def root_mean_square(data):
+    # requires that data is np.array
+    return np.sqrt(np.mean(data**2))
+
+
+### Frequency-Domain Features
+
+def to_fdomain(data, sampling_rate):
+    return np.abs(np.fft.fft(data))**2, np.fft.fftfreq(len(data), 1/sampling_rate)
+
+def peak_frequencies(power, freqs):
+    from scipy.signal import find_peaks
+    peak_indices = find_peaks(power)[0]
+    return freqs[peak_indices]
+
+def mean_frequency(power, freqs):
+    return np.sum(freqs*power)/np.sum(psd)
+
+def median_frequency(power, freqs):
+    # Sort the power spectrum and frequencies in ascending order
+    sorted_indices = np.argsort(power)
+    sorted_spectrum = power[sorted_indices]
+    sorted_frequencies = freqs[sorted_indices]
+    
+    # Calculate the cumulative sum of the power spectrum
+    cumulative_sum = np.cumsum(sorted_spectrum)
+    
+    # Find the index where cumulative sum crosses half of the total power
+    median_index = np.argmax(cumulative_sum >= np.sum(sorted_spectrum) / 2)
+    
+    # Get the median frequency
+    mdf = sorted_frequencies[median_index]
+    
+    return mdf
+
+
+
+
+### Time-Frequency Domain Features
+
+
+def calculate_wavelet_coefficients(data, wavelet='morl'):
+    import pywt
+    # Perform Continuous Wavelet Transform
+    coefficients, _ = pywt.cwt(data, scales=range(1, len(data)+1), wavelet=wavelet)
+    
+    return coefficients
+
+
+
+### Other
