328dd8f366ee27b0878756f85b09ebb595e95db1
jjjoelle <jfaybish@ucsd.edu>
2023-06-25 14:13:24 -0700

    mouse can be controlled by accelerometer

--- psychopy_gui_tests/accelerometer.py
+++ psychopy_gui_tests/accelerometer.py
@@ -8,6 +8,7 @@ from psychopy import event
 from psychopy import visual
 import threading
 import sys
+import pyautogui
 
 eeg_inlet = None
 buffer_x = []
@@ -21,21 +22,35 @@ def lsl_thread():
     global buffer_y
     global buffer_z
     
-    ch = 0
+    x = 0
+    z = 0
     
     print('LSL thread awake'); sys.stdout.flush();
+
     
     # Read LSL
+    # end loop after 10 seconds
+    start_time = time.time()
     while True:
+        
         sample, times = eeg_inlet.pull_sample()
-        # Append sample if exists (from single channel, ch) to file
-        if sample[ch] > 0:
-            out_path = prefix + "_data.txt"
-            '''buffer_x.append(sample[0])
-            buffer_y.append(sample[1])
-            buffer_z.append(sample[2])'''
-                #fo.write(f"{str(times)}, {str(sample)[1:-1]}\n")
+        
+        if sample[0] > 0.2: 
+            x = 1
+        elif sample[0] < -0.2:
+            x = -1
+        if sample[2] > 1.2:
+            z = 1
+        elif sample[2] < 0.8:
+            z = -1
+        
 
+        pyautogui.move(x, z)
+        x = 0
+        z = 0
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= 10:
+            break
 
 
 
@@ -44,6 +59,12 @@ if __name__ == "__main__":
     session = 0
 
     prefix = "/Users/jfaybishenko/projects/TNT/Project-TNNI-ACD/data/eeg_recordings/sess{}/".format(session) + 'accelerometer'
+
+    # initialize pyautogui window and set cursor in middle
+    screenWidth, screenHeight = pyautogui.size()
+    pyautogui.moveTo(screenWidth/2, screenHeight/2)
+
+
 
     # Initialize LSL marker/ stream
     # mrkstream = CreateMrkStream();
@@ -57,4 +78,4 @@ if __name__ == "__main__":
     lsl.setDaemon(True) 
     lsl.start()
     
-    time.sleep(10)
+    time.sleep(12)
