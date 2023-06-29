96189c8daa66160dfcf1bbdfdec00864f1e15b48
jjjoelle <jfaybish@ucsd.edu>
2023-06-28 21:42:40 -0700

    clean accelerometer code

--- psychopy_gui_tests/accelerometer.py
+++ psychopy_gui_tests/accelerometer.py
@@ -1,19 +1,11 @@
 import time
-#import pylsl-1.13.0.b4-py_0    #with conda install pyxdf and psychopy, and with pip install pylsl globally
 import pylsl
-import random
 import numpy as np
-import psychopy 
-from psychopy import event
-from psychopy import visual
 import threading
 import sys
 import pyautogui
 
 eeg_inlet = None
-buffer_x = []
-buffer_y = []
-buffer_z = []
 
 def lsl_thread():
     global eeg_inlet
@@ -22,18 +14,12 @@ def lsl_thread():
     
     # Read LSL
     while True:
-        
         sample, times = eeg_inlet.pull_sample()
         pyautogui.move(sample[0]*12,  sample[1] * 12)
 
 
 
 if __name__ == "__main__":
-    # SET GLOBALS 
-    session = 0
-
-    prefix = "/Users/jfaybishenko/projects/TNT/Project-TNNI-ACD/data/eeg_recordings/sess{}/".format(session) + 'accelerometer'
-
     # initialize pyautogui window and set cursor in middle
     screenWidth, screenHeight = pyautogui.size()
     pyautogui.moveTo(screenWidth/2, screenHeight/2)
@@ -41,8 +27,6 @@ if __name__ == "__main__":
 
 
     # Initialize LSL marker/ stream
-    # mrkstream = CreateMrkStream();
-    
     eeg_streams = pylsl.resolve_stream('type', 'EEG')
     eeg_inlet = pylsl.stream_inlet(eeg_streams[0], recover = False)
     print('Inlet Created'); sys.stdout.flush();
